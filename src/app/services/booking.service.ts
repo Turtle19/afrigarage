211836fc839887491cc99e115636fe2dc5c918e9
Aissatou Diallo <daissatoul97@gmail.com>
2021-10-28 17:15:39 +0200

    feat() build project architecture

---
+++ src/app/services/booking.service.ts
@@ -0,0 +1,21 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookingService {
+
+  constructor() { }
+
+  handleError(error: HttpErrorResponse){
+    if (error.error instanceof ErrorEvent) {
+      console.log('An error occured:', error.error.message);  
+    } else{
+      console.log(`Backend returned code ${error.status}, ` + `body was: ${error.error}`);
+    }
+    return throwError(error);
+  }
+}
+

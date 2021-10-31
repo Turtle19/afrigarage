d187747d52b8f5afd9f01915c12740797eae807d
Aissatou Diallo <daissatoul97@gmail.com>
2021-10-31 21:00:24 +0100

    add new route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BookingComponent } from './booking/booking.component';
 import { FavorisComponent } from './favoris/favoris.component';
+import { GarageDetailComponent } from './garage-detail/garage-detail.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
@@ -10,7 +11,8 @@ const routes: Routes = [{ path: 'login', component: LoginComponent },
 { path: 'accueil', component: HomeComponent },
 { path: 'reservation', component: BookingComponent },
 { path: 'favoris', component: FavorisComponent },
-{ path: '',   redirectTo: '/accueil', pathMatch: 'full' }, // redirect to `homeComponent`
+{ path: 'garage-detail/:id', component: GarageDetailComponent },
+{ path: '',   redirectTo: 'login', pathMatch: 'full' }, // redirect to `homeComponent`
 { path: '**', component: PageNotFoundComponent } ] // Wildcard route for a 404 page;
 
 @NgModule({

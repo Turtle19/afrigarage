13b45b1b25fe7058afe7cae5ef9ad490dba34219
Aissatou Diallo <daissatoul97@gmail.com>
2021-10-31 21:04:45 +0100

    implement errorService + garageService

--- src/app/services/error.service.ts
+++ src/app/services/error.service.ts
@@ -1,4 +1,6 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,14 @@ import { Injectable } from '@angular/core';
 export class ErrorService {
 
   constructor() { }
+
+  handleError(error: HttpErrorResponse){
+    if (error.error instanceof ErrorEvent) {
+      console.log("An error occured: ", error.error.message);
+    } else{
+      console.log(`Backend returned code ${error.status}, ` + `body was: ${error.error}`);
+      
+    }
+    return throwError(error);
+  }
 }
--- src/app/services/garage.service.ts
+++ src/app/services/garage.service.ts
@@ -1,9 +1,51 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Avis, Garage } from '../entities/garageEntity';
+import { ErrorService } from './error.service';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GarageService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private errorService: ErrorService) { }
+
+  getAllGarages(): Observable<Garage[]>{
+    return this.http.get<Garage[]>(`${environment.urlBack}/garages`).pipe(
+      map((response) => {
+        return response;
+      }),
+      catchError((err) => this.errorService.handleError(err))
+    );
+  }
+  
+  getGarageById(idGarage: number): Observable<Garage>{
+    return this.http.get<Garage>(`${environment.urlBack}/garages/${idGarage}`).pipe(
+      map((response) => {
+        return response;
+      }),
+      catchError((err) => this.errorService.handleError(err))
+    );
+  }
+
+  getGarageVote(idGarage: number): Observable<Avis[]>{
+    return this.http.get<Avis[]>(`${environment.urlBack}/avis?garage.id=${idGarage}`).pipe(
+      map((response) => {
+        return response;
+      }),
+      catchError((err) => this.errorService.handleError(err))
+    );
+  }
+
+  getGaragesBySearch(keyword: string): Observable<Garage[]>{
+    return this.http.get<Garage[]>(`${environment.urlBack}/garages?name_contains=${keyword}`).pipe(
+      map((response) => {
+        return response;
+      }),
+      catchError((err) => this.errorService.handleError(err))
+    );
+  }
 }

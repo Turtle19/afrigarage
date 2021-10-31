5187127fa944c3bf8a71cbc394baff8e9db6972d
Aissatou Diallo <daissatoul97@gmail.com>
2021-10-31 21:05:47 +0100

    add variables d'env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  urlBack: "http://localhost:1337",
+  urlAuth: "http://localhost:1337/auth/local",
+  urlRegister: "localhost:1337/auth/local/register"
 };
 
 /*

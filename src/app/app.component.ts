56359df3fdac7fae193e1bd43ecb38f63c4febff
Aissatou Diallo <daissatoul97@gmail.com>
2021-10-31 21:03:00 +0100

    gestion de la connexion & déconnexion+ ajout du token dans le localstorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  get isLoggedIn(){
+    return this.authService.isLoggedIn();
+  }
+
+  constructor(private authService: AuthService) {
+  }
+  ngOnInit(): void {
+    console.log(localStorage.getItem('id_token'));
+    
+  }
   title = 'afrigarage';
+  isConnected = false;
   
   navbarOpen = false; 
 
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { ErrorService } from './error.service';
+import { catchError, map } from 'rxjs/operators';
+import { AuthResponse } from '../entities/userEntity';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private http: HttpClient, private errorService: ErrorService) {}
 
-  constructor() { }
+  connect(login: string, password: string): Observable<AuthResponse> {
+    return this.http
+      .post<any>(`${environment.urlAuth}`, { identifier: login, password })
+      .pipe(
+        map((response) => {
+          localStorage.setItem('id_token', response.jwt);
+          return response;
+        }),
+        catchError((err) => this.errorService.handleError(err))
+      );
+  }
+
+  logout() {
+    localStorage.removeItem('id_token');
+  }
+
+  public isLoggedIn() {
+    return localStorage.getItem('id_token') !== null;
+  }
+
+  isLoggedOut() {
+    return !this.isLoggedIn();
+  }
 }

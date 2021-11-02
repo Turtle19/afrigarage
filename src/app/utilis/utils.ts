85ee401f0ceaeb6bd6acf74056744fc6129c5bfb
Aissatou Diallo <daissatoul97@gmail.com>
2021-11-02 23:58:37 +0100

    add utils enum

---
+++ src/app/utilis/utils.ts
@@ -0,0 +1,14 @@
+export enum DayOfWeek{
+    MONDAY = "Lundi",
+    TUESDAY = "Mardi",
+    WEDNESDAY = "Mercredi",
+    THURSDAY = "Jeudi",
+    FRIDAY = "Vendredi",
+    SATUDAY = "Samedi",
+    SUNDAY = "Diamnche",
+}
+
+export enum Status{
+    OPENED = "Ouvert",
+    CLOSED = "Fermé"
+}

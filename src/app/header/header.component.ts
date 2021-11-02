609646104a263fe5818c5d985f647c7da83815fe
Aissatou Diallo <daissatoul97@gmail.com>
2021-11-03 00:00:57 +0100

    gestion de la déconnexion

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,40 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Garage } from '../entities/garageEntity';
+import { AuthService } from '../services/auth.service';
 import { GarageService } from '../services/garage.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-
-  navbarOpen = false; 
+  navbarOpen = false;
   searchForm: FormGroup;
   garagesResult: Garage[] = [];
 
-  toggleNavbar() { 
-    this.navbarOpen = !this.navbarOpen; 
-  } 
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
 
-  constructor(private fb: FormBuilder, private garageService: GarageService) { 
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private garageService: GarageService,
+    private router: Router
+  ) {
     this.searchForm = this.fb.group({
       keyword: ['', Validators.required],
     });
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onSubmit() {
     console.log(this.searchForm.value);
-    this.garageService.getGaragesBySearch(this.searchForm.value.keyword).subscribe(garages => {
-      this.garagesResult = garages;
-      console.log(this.garagesResult.length);
-      
-    })
-    
+    this.garageService
+      .getGaragesBySearch(this.searchForm.value.keyword)
+      .subscribe((garages) => {
+        this.garagesResult = garages;
+        console.log(this.garagesResult.length);
+      });
   }
 
+  logout() {
+    console.log('logout');
+
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }

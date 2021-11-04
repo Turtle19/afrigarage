e3ebbb664c22ec2725136daeebd92e3a7252bd2b
Aissatou Diallo <daissatoul97@gmail.com>
2021-11-04 23:59:38 +0100

    wip affichage des service et ouverture de modale pour affichage des creneaux

--- src/app/garage-detail/garage-detail.component.ts
+++ src/app/garage-detail/garage-detail.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Garage } from '../entities/garageEntity';
 import { GarageService } from '../services/garage.service';
+import {MatDialog} from '@angular/material/dialog';
+import { DialogRDVComponent } from '../dialog-rdv/dialog-rdv.component';
+import { Service } from '../entities/serviceEntity';
 
 @Component({
   selector: 'app-garage-detail',
@@ -13,18 +16,29 @@ export class GarageDetailComponent implements OnInit {
   garage: Garage | undefined;
 
   constructor(private garageService: GarageService, private route: ActivatedRoute,
-    private router: Router) { }
+    private router: Router, public dialog: MatDialog) { }
 
   ngOnInit(): void {
     const garageId = this.route.snapshot.paramMap.get('id');
     if (garageId !== null) {
       this.garageService.getGarageById(parseInt(garageId, 10)).subscribe(garage => {
         this.garage = garage;
-        console.log(this.garage);
-        
       })
     }
-    
+  }
+
+  openDialogRdv(service: Service){
+    const dialogRef = this.dialog.open(DialogRDVComponent, {
+      width: '550px',
+      data: service,
+      hasBackdrop: false
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      
+    });
+
   }
 
 }

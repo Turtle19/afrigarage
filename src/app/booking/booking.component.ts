739aff3ce0aec9c3e244bee9f72c43f0999a39c8
Aissatou Diallo <daissatoul97@gmail.com>
2021-11-27 21:28:19 +0100

    add style & reformat file

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Booking } from '../entities/booking';
+import { environment } from 'src/environments/environment';
+import Swal from 'sweetalert2';
+import { Booking, Status } from '../entities/booking';
 import { BookingService } from '../services/booking.service';
 
 @Component({
@@ -10,6 +12,8 @@ import { BookingService } from '../services/booking.service';
 export class BookingComponent implements OnInit {
 
   bookings: Booking[] = [];
+  url = environment.urlBack;
+  status = Status;
 
   constructor(private bookingService: BookingService) { }
 
@@ -18,7 +22,32 @@ export class BookingComponent implements OnInit {
     if (idUser !== null) {
       this.bookingService.getUserBooking(parseInt(idUser, 10)).subscribe(bookings => this.bookings = bookings)
     }
-    
   }
 
+  cancelBooking(booking: Booking){
+    let idUser = localStorage.getItem('id_user');
+    booking.user = null;
+    booking.status = Status.CANCELED;
+    Swal.fire({
+      title: 'Souhaitez-vous annuler le rendez-vous?',
+      showCancelButton: true,
+      confirmButtonText: 'Confirmer',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.bookingService.updateBooking(booking).subscribe(res => {
+          if (res !== null && idUser !== null) {
+            this.bookingService.getUserBooking(parseInt(idUser, 10)).subscribe(bookings => this.bookings = bookings);
+            Swal.fire('Annulation effectuée avec succès!', '', 'success');
+          }
+        })
+      } else {
+        Swal.fire('Aucune modification n\'a été effectuée', '', 'info')
+      }
+    })
+  }
+
+  dateIsBefore(booking: Booking){
+    const date = new Date(booking.date);
+    return date.getTime() < new Date().getTime();
+  }
 }

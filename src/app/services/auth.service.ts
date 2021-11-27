46c7440598777a895ed08f007a22f71adc5e0eaf
Aissatou Diallo <daissatoul97@gmail.com>
2021-11-28 00:33:27 +0100

    feat() implement methode register pour inscrire un user via dans la bdd

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,16 +1,23 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { ErrorService } from './error.service';
 import { catchError, map } from 'rxjs/operators';
-import { AuthResponse } from '../entities/userEntity';
+import { AuthResponse, User } from '../entities/userEntity';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   constructor(private http: HttpClient, private errorService: ErrorService) {}
+
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept': '*/*'
+    })
+  }
 
   connect(login: string, password: string): Observable<AuthResponse> {
     return this.http
@@ -19,6 +26,17 @@ export class AuthService {
         map((response: AuthResponse) => {
           localStorage.setItem('id_token', response.jwt);
           localStorage.setItem('id_user', response.user.id.toString());
+          return response;
+        }),
+        catchError((err) => this.errorService.handleError(err))
+      );
+  }
+
+  register(newUser: any): Observable<User> {
+    return this.http
+      .post<User>(`${environment.urlRegister}`, newUser, this.httpOptions)
+      .pipe(
+        map((response: User) => {
           return response;
         }),
         catchError((err) => this.errorService.handleError(err))
